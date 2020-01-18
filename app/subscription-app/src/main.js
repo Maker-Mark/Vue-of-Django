7f26b3ac5c333370e77c85f9659d537f5b0c4b73
Mark Goldstein <markgoldstein12@gmail.com>
2020-01-18 14:19:12 -0500

    added router and connection to sub api

--- app/subscription-app/src/main.js
+++ app/subscription-app/src/main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstapVue from 'bootstrap-vue' 
-import VeeValidate from 'vee-validate'
+import {ValidationProvider} from 'vee-validate';
+import router from './router'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 
 Vue.use(BootstapVue)
-Vue.use(VeeValidate)
+Vue.component('ValidationProvider', ValidationProvider);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
 
--- app/subscription-app/src/router.js
+++ app/subscription-app/src/router.js
@@ -1,28 +1,28 @@
-import Vue from "vue"
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path:"/",
-            redirect:'/index'
-        },
-        {
-            path:'/create',
-            name:"create",
-            component: () => import('./components/Create.vue')
-        },
-        {
-          path: "/edit/:id",
-          name: "edit",
-          component: () => import("./components/Edit.vue")
-        },
-        {
-          path: "/index",
-          name: "index",
-          component: () => import("./components/Index.vue")
-        },
-    ]
-})
+  routes: [
+    {
+      path: "/",
+      redirect: '/index'
+    },
+    {
+      path: "/create",
+      name: "create",
+      component: () => import("./components/Create.vue")
+    },
+    {
+      path: "/edit/:id",
+      name: "edit",
+      component: () => import("./components/Edit.vue")
+    },
+    {
+      path: "/index",
+      name: "index",
+      component: () => import("./components/Index.vue")
+    },
+  ]
+});
